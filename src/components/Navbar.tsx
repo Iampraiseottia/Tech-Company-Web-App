d0658e4aacec88ca41a67d31763665a68924a939
OTTIA PRAISE BETECK <ottiapraise50@gmail.com>
2024-04-12 15:04:38 -1200

    Navbar completed

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+
+const Navbar = () => {
+  return (
+    <section id='header' className='flex justify-between items-center h-auto py-10 text-white px-9'>
+        <div className="flex items-center">   
+            <img src="./src/assets/companyLogo.png" className="rounded-full h-14 w-14" />
+            <h1 className='font-bold text-2xl pl-3 tracking-wider hover:select-none'>OTTIATECH</h1>
+        </div>
+        <nav >
+            <ul className="flex items-center">
+                <li><a href="#" className="text-xl px-3 hover:opacity-80" id="Home">Home</a></li>
+                <li><a href="#" className="text-xl px-3 hover:opacity-80">About</a></li>
+                <li><a href="#" className="text-xl px-3 hover:opacity-80">Service</a></li>
+                <li><a href="#" className="text-xl px-3 hover:opacity-80">Team</a></li>
+                <li><a href="#" className="text-xl px-3 hover:opacity-80">Testimony</a></li>
+                <li><a href="#" className="text-xl px-3 mr-3 hover:opacity-80">Contact</a></li>
+                <button className="border-2  py-3 px-7 font-bold rounded-3xl hover:bg-blue-600 hover:border-blue-600 hover:opacity-90"><a href="">Sign In</a></button>
+            </ul>
+        </nav>
+    </section>
+  )
+}
+
+export default Navbar
