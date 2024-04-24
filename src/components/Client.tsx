294f4120a6d863f837c575ff150561bd3e69937c
OTTIA PRAISE BETECK <ottiapraise50@gmail.com>
2024-04-23 19:05:47 -1200

    clients completed

--- src/components/Client.tsx
+++ src/components/Client.tsx
@@ -2,8 +2,13 @@
 const Client = () => {
 
   return (
-    <section id="Client" className="">
-
+    <section id="Client" className="my-11 mx-8 flex items-center justify-evenly">
+      <img src="./src/assets/client-1.png" className="h-6 w-auto hover:h-[24.4px] hover:cursor-pointer" id="ClientImg" />
+      <img src="./src/assets/client-2.png" className="h-6 w-auto hover:h-[24.4px] hover:cursor-pointer" id="ClientImg" />
+      <img src="./src/assets/client-3.png" className="h-6 w-auto hover:h-[24.4px] hover:cursor-pointer" id="ClientImg" />
+      <img src="./src/assets/client-4.png" className="h-6 w-auto hover:h-[24.4px] hover:cursor-pointer" id="ClientImg" />
+      <img src="./src/assets/client-5.png" className="h-6 w-auto hover:h-[24.4px] hover:cursor-pointer" id="ClientImg" />
+      <img src="./src/assets/client-6.png" className="h-6 w-auto hover:h-[24.4px] hover:cursor-pointer" id="ClientImg" />
     </section>
   )
 }
